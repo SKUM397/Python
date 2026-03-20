dcd0bb13dae0eb3732820830c600a85d6cc2128f
SKUM397 <shudhanshu67.sk@gmail.com>
2026-03-20 21:39:39 +0530

    Initial push

---
+++ Hello world.py
@@ -0,0 +1,10 @@
+#Author :: Sudhanshu Kumar
+#location :: India
+# Date :: 17/10/2022
+
+from playsound import playsound
+import os
+
+print ("Hello world")
+playsound('G:\codes\python\ponus.mp3')
+print(os.listdir())
